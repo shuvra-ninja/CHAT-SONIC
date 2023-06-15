38253bd3008cf4bb974d67babdfa1d87082ced99
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-06-15 08:31:38 +0530

    change for init deploy

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const path = require("path");
-
+require("dotenv").config();
 const app = express();
+const port = "3000";
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 const publicRoutes = require("./routes/public");
 
 app.use(publicRoutes);
 
-app.listen("3000");
+app.listen(process.env.PORT || port, () => {
+  console.log(`listning to the port ${port}`);
+});
